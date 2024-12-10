df40c5f5c256278a45b4642fca341e68bfc4d208
abhishek-ays <166599412+abhishek-ays@users.noreply.github.com>
2024-12-10 19:19:55 +0530

    added transition on page changes

--- src/components/AppRoutes.jsx
+++ src/components/AppRoutes.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import MainPage from './MainPage';
 import ESignWorkflow from './Feature pages/ESignWorkflow';
 import BusinessWorkflow from './Feature pages/BusinessWorkflow';
 import DailyDiary from './Feature pages/DailyDiary';
 import Certification from './Certification';
 import Layout from './Layout';
-
+import '../components/Styling/AppRoutes.css';
 
 const AppRoutes = () => {
+    const location = useLocation();
+
     return (
-        <Routes>
-            <Route path="/" element={<Layout><MainPage /></Layout>} />
-            <Route path="/esignWorkflow" element={<Layout><ESignWorkflow /></Layout>} />
-            <Route path="/businessWorkflow" element={<Layout><BusinessWorkflow /></Layout>} />
-            <Route path="/dailyDiary" element={<Layout><DailyDiary /></Layout>} />
-            <Route path="/IGBCCertification" element={<Layout><Certification /></Layout>} />
-        </Routes>
+        <TransitionGroup>
+            <CSSTransition key={location.key} classNames="slide-fade" timeout={600}>
+                <Routes location={location}>
+                    <Route path="/" element={<Layout><MainPage /></Layout>} />
+                    <Route path="/esignWorkflow" element={<Layout><ESignWorkflow /></Layout>} />
+                    <Route path="/businessWorkflow" element={<Layout><BusinessWorkflow /></Layout>} />
+                    <Route path="/dailyDiary" element={<Layout><DailyDiary /></Layout>} />
+                    <Route path="/IGBCCertification" element={<Layout><Certification /></Layout>} />
+                </Routes>
+            </CSSTransition>
+        </TransitionGroup>
     );
-}
+};
 
 export default AppRoutes;
