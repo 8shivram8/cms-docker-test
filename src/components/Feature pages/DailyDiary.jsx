7072598c960295b0e552a2a84322dbf83576c391
abhishek-ays <166599412+abhishek-ays@users.noreply.github.com>
2024-12-13 17:36:31 +0530

    added home link on coalitionify text

--- src/components/Feature pages/DailyDiary.jsx
+++ src/components/Feature pages/DailyDiary.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
-
+import { Box, Typography } from '@mui/material'
 const DailyDiary = () => {
   return (
-    <div>
-      DailyDiary
-    </div>
+    <Box 
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#f5f5f5',
+        // height:'auto'
+        flex:1
+      }}
+    >
+      <Typography 
+        variant="h3" 
+        sx={{
+          fontWeight: 'bold', 
+          textAlign: 'center',
+          color: 'primary.main'
+        }}
+      >
+        Coming Soon...
+      </Typography>
+    </Box>
   )
 }
 
