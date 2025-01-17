46c0166d6000852697950216a907623ac4d97faa
abhishek-ays <abhishek.shinde@ayssoftwaresolution.com>
2025-01-17 14:34:22 +0530

    added footer changes

--- src/new components/Footer.jsx
+++ src/new components/Footer.jsx
@@ -2,18 +2,20 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import InstagramIcon from '@mui/icons-material/Instagram';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import { useMediaQuery, useTheme } from '@mui/material';
+import Grid from '@mui/material/Grid';
 import Divider from '@mui/material/Divider';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+
 
 function Footer() {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
   const isTablet = useMediaQuery(theme.breakpoints.between("sm", "md"));
-
+  const handleLinkClick = (url) => {
+    window.open(url, '_blank');
+  };
   return (
     <Box
       component="footer"
@@ -24,56 +26,60 @@ function Footer() {
         boxShadow: 3,
       }}
     >
-      <Box
-        display="flex"
-        flexDirection={isMobile ? 'column' : 'row'}
-        justifyContent="space-between"
-        alignItems={isMobile ? 'center' : 'flex-start'}
-        gap={isMobile ? 2 : 4}
-      >
+      <Grid container spacing={isMobile ? 2 : 4} justifyContent="center">
         {/* Left Section */}
-        <Box ml={isMobile ? 2 : isTablet ? 5 : 10} textAlign={isMobile ? 'left' : 'left'} display={'flex'} flexDirection={'column'} gap={2}>
-          <Typography fontWeight="bold" fontSize={'25px'} >Coalitionify</Typography>
-          {!isMobile ? (
+        <Grid item xs={12} sm={6} md={3} lg={3} textAlign={isMobile ? 'center' : 'left'}>
+          <Box display="flex" flexDirection="column" alignItems={isMobile ? 'center' : 'flex-start'}>
+            {/* <img src={logo} alt="Colitionify Logo" height={40} style={{ marginBottom: '10px' }} /> */}
+            <Typography fontWeight="bold" fontSize={'25px'} mb={1} >Coalitionify</Typography>
             <Typography
               variant={'caption'}
               fontWeight={400}
               fontSize={'14px'}
               sx={{ color: '#939AAD' }}
             >
-              We’re here to help! Whether you have questions about <br />
-              our services, need support, or want to discuss a potential <br />
+              We’re here to help! Whether you have questions about
+              our services, need support, or want to discuss a potential
               project, feel free to reach out.
             </Typography>
-          ) :
-            (
-              <Typography
-                variant={'caption'}
-                fontWeight={400}
-                fontSize={'14px'}
-                sx={{ color: '#939AAD' }}
-              >
-                We’re here to help! Whether you have questions about
-                our services, need support, or want to discuss a potential
-                project, feel free to reach out.
-              </Typography>
-            )
-          }
 
-        </Box>
-
-        {/* Right Section */}
-        <Box mr={isMobile ? 2 : isTablet ? 5 : 10} textAlign={!isMobile ? 'left' : 'center'}>
-          <Typography variant="h6" gutterBottom>Contact</Typography>
-          <Box>
-            <Typography variant="body2">Phone: (123) 456-7890</Typography>
-            <Typography variant="body2" mt={1}>Email: contact@company.com</Typography>
           </Box>
-        </Box>
-      </Box>
+        </Grid>
 
-      <Divider sx={{ mt: 3, borderColor: 'gray',mb:isMobile ? 2 :1 }} />
+        <Grid item xs={6} sm={6} md={3} textAlign={isMobile ? 'center' : 'left'}>
+          <Typography variant="h6" gutterBottom>Quick Links</Typography>
+          <Box display="flex" flexDirection="column" alignItems={isMobile ? 'center' : 'flex-start'} sx={{ lineHeight: 1.5 }}>
+            <span onClick={() => handleLinkClick('/privacy')} style={{ textDecoration: 'none', color: 'inherit', cursor: 'pointer' }}>Privacy Policy</span>
+            <span onClick={() => handleLinkClick('/terms-conditions')} style={{ textDecoration: 'none', color: 'inherit', cursor: 'pointer' }}>Terms & Conditions</span>
+            <span onClick={() => handleLinkClick('/refund-policy')} style={{ textDecoration: 'none', color: 'inherit', cursor: 'pointer' }}>Cancellation/ Refund Policy</span>
+          </Box>
+        </Grid>
 
+        {/* Address Section */}
+        <Grid item xs={6} sm={6} md={3} textAlign={isMobile ? 'center' : 'left'}>
+          <Typography variant="h6" gutterBottom>Address</Typography>
+          <Box display="flex" flexDirection="column" alignItems={isMobile ? 'center' : 'flex-start'} sx={{ lineHeight: 1.5 }}>
+            <Typography variant="body2">1234 Street Name</Typography>
+            <Typography variant="body2">City, State, 12345</Typography>
+            <Typography variant="body2">5678 Another St</Typography>
+            <Typography variant="body2">Another City, State, 67890</Typography>
+          </Box>
+        </Grid>
+
+        {/* Contact Section */}
+        <Grid item xs={6} sm={6} md={3} textAlign={isMobile ? 'center' : 'left'}>
+          <Typography variant="h6" gutterBottom>Contact</Typography>
+          <Box display="flex" flexDirection="column" alignItems={isMobile ? 'center' : 'flex-start'}>
+            <Typography variant="body2">Phone: (123) 456-7890</Typography>
+            <Typography variant="body2">Email: contact@company.com</Typography>
+          </Box>
+        </Grid>
+      </Grid>
+
+      <Divider sx={{ my: 2, borderColor: 'gray' }} />
+      {/* <Typography variant="body2" color="#fff" align="center">
+        © {new Date().getFullYear()} AYS Software Solution. All rights reserved.
+      </Typography> */}
       <Box
         display="flex"
         flexDirection={isMobile ? 'column' : 'row'}
@@ -99,30 +105,7 @@ function Footer() {
             justifyContent: isMobile ? 'center' : 'flex-start',
           }}
         >
-          <IconButton
-            sx={{ color: '#767E94' }}
-            component="a"
-            href="https://facebook.com"
-            target="_blank"
-          >
-            <FacebookIcon />
-          </IconButton>
-          <IconButton
-            sx={{ color: '#767E94' }}
-            component="a"
-            href="https://twitter.com"
-            target="_blank"
-          >
-            <TwitterIcon />
-          </IconButton>
-          <IconButton
-            sx={{ color: '#767E94' }}
-            component="a"
-            href="https://instagram.com"
-            target="_blank"
-          >
-            <InstagramIcon />
-          </IconButton>
+         
           <IconButton
             sx={{ color: '#767E94' }}
             component="a"
@@ -131,9 +114,16 @@ function Footer() {
           >
             <GitHubIcon />
           </IconButton>
+          <IconButton
+            sx={{ color: '#767E94' }}
+            component="a"
+            href="https://youtube.com"
+            target="_blank"
+          >
+             <YouTubeIcon />
+          </IconButton>
         </Box>
       </Box>
-
     </Box>
   );
 }
