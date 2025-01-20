5e6bec85f982773ad74bcc7e6127f61cbe7020c7
abhishek-ays <abhishek.shinde@ayssoftwaresolution.com>
2025-01-20 18:10:38 +0530

    added key features

--- src/new components/KeyFeaturesNew.jsx
+++ src/new components/KeyFeaturesNew.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Grid, Box, Typography } from '@mui/material'
-
+import FirstKey from './Custom Icons/FirstKey'
+import Key from './Custom Icons/Key'
 const features = [
   { title: 'Dynamic Form Builder', subtitle: 'Easily create rich forms for every use case, add multimedia input on top of your document. Build multistep wizard for data collection.' },
   { title: 'Secure E-Signatures', subtitle: 'Collect legally binding signatures with audit trails, encryption, and QR scan. Bulk trigger, bulk signing, participant group and lot more to streamline your process.' },
@@ -10,12 +11,18 @@ const features = [
 
 const KeyFeaturesNew = () => {
   return (
-    <Box bgcolor="#1677F7" py={3} mt={7}>
+    <Box bgcolor="#1677F7" py={3} mt={7} position={'relative'}>
+        <Box position="absolute" top={0} left={0}>
+        <FirstKey />
+      </Box>
+      <Box position="absolute" top={0} right={0}>
+        <Key />
+      </Box>
       <Typography variant="h4" align="center" color="white" sx={{ fontWeight:700}}>
         Key Features
       </Typography>
       <Box display="flex" justifyContent="center">
-        <Grid container spacing={2} justifyContent="center" maxWidth="lg">
+        <Grid container justifyContent="center" maxWidth="lg">
           {features.map((feature, index) => (
             <Grid item xs={6} sm={6} md={3} key={index}>
               <Box textAlign="left" p={3}>
