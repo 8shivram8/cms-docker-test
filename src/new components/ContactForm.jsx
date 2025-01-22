fb0528bafcdece9f71f32220b1c7779773c45f31
abhishek-ays <abhishek.shinde@ayssoftwaresolution.com>
2025-01-22 18:39:21 +0530

    handle contact form responce

--- src/new components/ContactForm.jsx
+++ src/new components/ContactForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     Typography,
     Grid,
@@ -12,10 +12,12 @@ import * as Yup from 'yup';
 import StyledTextfield from './StyledTextfield';
 import StyledMultiline from './StyledMultiline';
 import ArrowIcon from './Custom Icons/ArrowIcon';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 
 const ContactForm = () => {
     const theme = useTheme();
     const isSmallScreen = useMediaQuery(theme.breakpoints.down('md'));
+    const [isFormSubmitted, setIsFormSubmitted] = useState(false);
 
     const validationSchema = Yup.object({
         name: Yup.string().required('Name is required'),
@@ -48,6 +50,7 @@ const ContactForm = () => {
 
                 if (response.ok) {
                     resetForm();
+                    setIsFormSubmitted(true);
                 } else {
                     console.error('API call failed');
                 }
@@ -65,11 +68,12 @@ const ContactForm = () => {
                 alignItems: 'center',
                 width: '100%',
                 backgroundColor: '#f3f4f6',
-                mt: 2
+                mt: 2,
+
             }}
         >
-            <Grid container sx={{ maxWidth: 'lg', width: '100%' }} spacing={2} justifyContent="center" mt={2} >
-                <Grid item xs={12} md={6} >
+            <Grid container sx={{ maxWidth: 'lg', width: '100%' }} spacing={2} justifyContent="center" mt={2}>
+                <Grid item xs={12} md={6}>
                     <Box
                         sx={{
                             display: 'flex',
@@ -102,10 +106,12 @@ const ContactForm = () => {
                             Any Questions? <br /> Write or Call us. <br /> We will write back <br /> within 12 Hrs
                         </Typography>
                         <Box
+                            mb={isSmallScreen ? 3 : 3}
                             sx={{
                                 display: 'flex',
                                 alignItems: 'center',
                                 mt: 6,
+
                             }}
                         >
                             <ArrowIcon
@@ -120,6 +126,7 @@ const ContactForm = () => {
                                 fontWeight="light"
                                 color="text.secondary"
                                 ml={isSmallScreen ? 3 : 4}
+
                             >
                                 info@coalitionify.com
                             </Typography>
@@ -128,79 +135,123 @@ const ContactForm = () => {
                 </Grid>
 
                 <Grid item xs={12} md={5}>
-                    <Box
-                        component="form"
-                        sx={{
-                            display: 'flex',
-                            flexDirection: 'column',
-                            m: 3,
-                            background: 'linear-gradient(215.51deg, #1677F7 0%, #63A6FF 64.27%, #D9E9FF 107.12%)',
-                            borderRadius: 2,
-                        }}
-                        onSubmit={formik.handleSubmit}
-                    >
-                        <Box m={3} gap={2} display={'flex'} flexDirection={'column'}>
-                            <StyledTextfield
-                                name="name"
-                                type="text"
-                                placeholder="Name"
-                                value={formik.values.name}
-                                onChange={formik.handleChange}
-                                onBlur={formik.handleBlur}
-                                error={formik.touched.name && Boolean(formik.errors.name)}
-                                helperText={formik.touched.name && formik.errors.name}
-                                sx={{ mb: 1 }}
-                            />
-                            <StyledTextfield
-                                name="email"
-                                type="email"
-                                placeholder="Email"
-                                value={formik.values.email}
-                                onChange={formik.handleChange}
-                                onBlur={formik.handleBlur}
-                                error={formik.touched.email && Boolean(formik.errors.email)}
-                                helperText={formik.touched.email && formik.errors.email}
-                                sx={{ mb: 1 }}
-                            />
-                            <StyledTextfield
-                                name="phone"
-                                type="tel"
-                                placeholder="Phone Number (Optional)"
-                                value={formik.values.phone}
-                                onChange={formik.handleChange}
-                                onBlur={formik.handleBlur}
-                                error={formik.touched.phone && Boolean(formik.errors.phone)}
-                                helperText={formik.touched.phone && formik.errors.phone}
-                                sx={{ mb: 1 }}
-                            />
-                            <StyledMultiline
-                                name="message"
-                                multiline
-                                rows={4}
-                                placeholder="Message (Optional)"
-                                value={formik.values.message}
-                                onChange={formik.handleChange}
-                                onBlur={formik.handleBlur}
-                                error={formik.touched.message && Boolean(formik.errors.message)}
-                                helperText={formik.touched.message && formik.errors.message}
-                                sx={{ mb: 1 }}
-                            />
-                            <Button
-                                type="submit"
-                                variant="contained"
-                                color="black"
-                                size="large"
+                    {isFormSubmitted ? (
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                background: 'linear-gradient(215.51deg, #1677F7 0%, #63A6FF 64.27%, #D9E9FF 107.12%)',
+                                borderRadius: 2,
+                                p: 3,
+                                m: 3,
+                                mt: 6
+                            }}
+                        >
+                            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', mb: 2 }}>
+                                <CheckCircleIcon sx={{ color: 'green', fontSize: '40px' }} />
+                                <Typography
+                                    mt={2}
+                                    variant="h6"
+                                    sx={{
+                                        fontWeight: 700,
+                                        textAlign: 'center',
+                                    }}
+                                >
+                                    Form Submitted Successfully
+                                </Typography>
+                            </Box>
+                            <Typography
+                                mt={2}
+                                variant="h6"
+                                fontWeight={500}
+                                fontSize={isSmallScreen ? '14px' : '18px'}
                                 sx={{
-                                    textTransform: 'none',
-                                    fontSize: '1rem',
-                                    backgroundColor: 'black',
-                                    color: 'white',
+                                    lineHeight: '1.5',
+                                    textAlign: 'center',
+                                    color: 'black',
+                                    maxWidth: '500px',
                                 }}
                             >
-                                Submit
-                            </Button>
+                                Your request has been received, and our team is currently reviewing it. We will get back to you within 12 hours.
+                            </Typography>
                         </Box>
-                    </Box>
+                    ) : (
+                        <Box
+                            component="form"
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                m: 3,
+                                background: 'linear-gradient(215.51deg, #1677F7 0%, #63A6FF 64.27%, #D9E9FF 107.12%)',
+                                borderRadius: 2,
+                            }}
+                            onSubmit={formik.handleSubmit}
+                        >
+                            <Box m={3} gap={2} display={'flex'} flexDirection={'column'}>
+                                <StyledTextfield
+                                    name="name"
+                                    type="text"
+                                    placeholder="Name"
+                                    value={formik.values.name}
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    error={formik.touched.name && Boolean(formik.errors.name)}
+                                    helperText={formik.touched.name && formik.errors.name}
+                                    sx={{ mb: 1 }}
+                                />
+                                <StyledTextfield
+                                    name="email"
+                                    type="email"
+                                    placeholder="Email"
+                                    value={formik.values.email}
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    error={formik.touched.email && Boolean(formik.errors.email)}
+                                    helperText={formik.touched.email && formik.errors.email}
+                                    sx={{ mb: 1 }}
+                                />
+                                <StyledTextfield
+                                    name="phone"
+                                    type="tel"
+                                    placeholder="Phone Number (Optional)"
+                                    value={formik.values.phone}
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    error={formik.touched.phone && Boolean(formik.errors.phone)}
+                                    helperText={formik.touched.phone && formik.errors.phone}
+                                    sx={{ mb: 1 }}
+                                />
+                                <StyledMultiline
+                                    name="message"
+                                    multiline
+                                    rows={4}
+                                    placeholder="Message (Optional)"
+                                    value={formik.values.message}
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    error={formik.touched.message && Boolean(formik.errors.message)}
+                                    helperText={formik.touched.message && formik.errors.message}
+                                    sx={{ mb: 1 }}
+                                />
+                                <Button
+                                    type="submit"
+                                    variant="contained"
+                                    color="black"
+                                    size="large"
+                                    sx={{
+                                        textTransform: 'none',
+                                        fontSize: '1rem',
+                                        backgroundColor: 'black',
+                                        color: 'white',
+                                    }}
+                                >
+                                    Submit
+                                </Button>
+                            </Box>
+                        </Box>
+                    )}
                 </Grid>
             </Grid>
         </Box>
