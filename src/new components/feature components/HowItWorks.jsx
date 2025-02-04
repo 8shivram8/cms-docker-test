f3f61c913117c9c9d303cb8340d605e31e1bacf6
abhishek-ays <abhishek.shinde@ayssoftwaresolution.com>
2025-02-04 11:35:03 +0530

    responsive changes

--- src/new components/feature components/HowItWorks.jsx
+++ src/new components/feature components/HowItWorks.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Grid, Box, Typography, Divider, List, ListItem, ListItemIcon, ListItemText } from "@mui/material";
+import { Container, Grid, Box, Typography, Divider, List, ListItem, ListItemIcon, ListItemText, useMediaQuery, useTheme } from "@mui/material";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
 const leadRouting = {
@@ -39,6 +39,9 @@ const leadRouting2 = {
 };
 
 const HowItWorks = () => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm", "xs"));
+    const isTablet = useMediaQuery(theme.breakpoints.between("sm", "md"));
   return (
     <Container maxWidth="lg">
       <Grid container spacing={1}>
@@ -50,52 +53,58 @@ const HowItWorks = () => {
           </Box>
         </Grid>
         <Grid item xs={12}>
-        <Box display="flex" justifyContent="center" alignItems="center" width="100%" border={'1px solid black'} >
-            <Box display="flex" width="88%" sx={{ minHeight: "150px" }} mt={2} >
-              <Box p={2} flex={1} textAlign="left" sx={{ pr: 2 }}>
-                <Typography variant="h6" fontWeight={600} mb={1}>
-                  {leadRouting.title}
-                </Typography>
-                <List dense>
-                  {leadRouting.details.map((item, index) => (
-                    <ListItem key={index} sx={{ alignItems: "flex-start" }}>
-                      <ListItemIcon sx={{ minWidth: 30, mt: 0.5 }}>
-                        <CheckCircleIcon sx={{ color: "black", fontSize: 18 }} />
-                      </ListItemIcon>
-                      <ListItemText
-                        primary={item}
-                        primaryTypographyProps={{ variant: "body2" }}
-                        sx={{ textAlign: "left", ml: 0.5 }}
-                      />
-                    </ListItem>
-                  ))}
-                </List>
-              </Box>
-              <Divider orientation="vertical" flexItem sx={{ bgcolor: "grey.400", height: "150px", alignSelf: "center" }} />
-              <Box p={2} flex={1} textAlign="left" sx={{ pl: 4 }}>
-                <Typography variant="h6" fontWeight={400} mb={1}>
-                  {leadScoring1.title}
-                </Typography>
-                <List dense>
-                  {leadScoring1.details.map((item, index) => (
-                    <ListItem key={index} sx={{ alignItems: "flex-start" }}>
-                      <ListItemIcon sx={{ minWidth: 30, mt: 0.5 }}>
-                        <CheckCircleIcon sx={{ color: "black", fontSize: 18 }} />
-                      </ListItemIcon>
-                      <ListItemText
-                        primary={item}
-                        primaryTypographyProps={{ variant: "body2" }}
-                        sx={{ textAlign: "left", ml: 0.5 }}
-                      />
-                    </ListItem>
-                  ))}
-                </List>
-              </Box>
-            </Box>
+      <Box display="flex" flexDirection="column" alignItems="center" width="100%" border={'1px solid black'}>
+        <Box display="flex" flexDirection={isMobile ? "column" : "row"} width="88%" sx={{ minHeight: "150px" }} mt={2}>
+          <Box p={2} flex={1} textAlign="left" sx={{ pr: isMobile ? 0 : 2, width: "100%" }}>
+            <Typography variant="h6" fontWeight={600} mb={1}>
+              {leadRouting.title}
+            </Typography>
+            <List dense>
+              {leadRouting.details.map((item, index) => (
+                <ListItem key={index} sx={{ alignItems: "flex-start" }}>
+                  <ListItemIcon sx={{ minWidth: 30, mt: 0.5 }}>
+                    <CheckCircleIcon sx={{ color: "black", fontSize: 18 }} />
+                  </ListItemIcon>
+                  <ListItemText
+                    primary={item}
+                    primaryTypographyProps={{ variant: "body2" }}
+                    sx={{ textAlign: "left", ml: 0.5 }}
+                  />
+                </ListItem>
+              ))}
+            </List>
           </Box>
-        </Grid>
+
+          {isMobile ? (
+            <Divider sx={{ bgcolor: "grey.400", my: 2 }} />
+          ) : (
+            <Divider orientation="vertical" flexItem sx={{ bgcolor: "grey.400", height: "150px", alignSelf: "center" }} />
+          )}
+
+          <Box p={2} flex={1} textAlign="left" sx={{ pl: isMobile ? 0 : 4, width: "100%" }}>
+            <Typography variant="h6" fontWeight={400} mb={1}>
+              {leadScoring1.title}
+            </Typography>
+            <List dense>
+              {leadScoring1.details.map((item, index) => (
+                <ListItem key={index} sx={{ alignItems: "flex-start" }}>
+                  <ListItemIcon sx={{ minWidth: 30, mt: 0.5 }}>
+                    <CheckCircleIcon sx={{ color: "black", fontSize: 18 }} />
+                  </ListItemIcon>
+                  <ListItemText
+                    primary={item}
+                    primaryTypographyProps={{ variant: "body2" }}
+                    sx={{ textAlign: "left", ml: 0.5 }}
+                  />
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+        </Box>
+      </Box>
+    </Grid>
         <Grid container item xs={12} spacing={3} alignItems="stretch" justifyContent="space-between" mt={0.2}>
-          <Grid item xs={6}>
+          <Grid item xs={12} md={6}>
             <Box p={3} height="80%" border={'1px solid black'}>
               <Typography variant="h6" fontWeight={600} mb={1}>
                 {leadSubmission.title}
@@ -103,21 +112,21 @@ const HowItWorks = () => {
               <List dense>
                 {leadSubmission.details.map((item, index) => (
                   <ListItem key={index} sx={{ alignItems: "flex-start" }}>
-                  <ListItemIcon sx={{ minWidth: 30, mt: 0.5 }}>
-                    <CheckCircleIcon sx={{ color: "black", fontSize: 18 }} />
-                  </ListItemIcon>
-                  <ListItemText
-                    primary={item}
-                    primaryTypographyProps={{ variant: "body2" }}
-                    sx={{ textAlign: "left", ml: 0.5 }}
-                  />
-                </ListItem>
+                    <ListItemIcon sx={{ minWidth: 30, mt: 0.5 }}>
+                      <CheckCircleIcon sx={{ color: "black", fontSize: 18 }} />
+                    </ListItemIcon>
+                    <ListItemText
+                      primary={item}
+                      primaryTypographyProps={{ variant: "body2" }}
+                      sx={{ textAlign: "left", ml: 0.5 }}
+                    />
+                  </ListItem>
                 ))}
               </List>
             </Box>
           </Grid>
           {/* <Divider orientation="vertical" flexItem sx={{ bgcolor: "grey.400", height: "60%", alignSelf: "center" }} /> */}
-          <Grid item xs={6}>
+          <Grid item xs={12} md={6}>
             <Box p={3} height="80%" border={'1px solid black'}>
               <Typography variant="h6" fontWeight={600} mb={1}>
                 {leadRouting2.title}
@@ -125,15 +134,15 @@ const HowItWorks = () => {
               <List dense>
                 {leadRouting2.details.map((item, index) => (
                   <ListItem key={index} sx={{ alignItems: "flex-start" }}>
-                  <ListItemIcon sx={{ minWidth: 30, mt: 0.5 }}>
-                    <CheckCircleIcon sx={{ color: "black", fontSize: 18 }} />
-                  </ListItemIcon>
-                  <ListItemText
-                    primary={item}
-                    primaryTypographyProps={{ variant: "body2" }}
-                    sx={{ textAlign: "left", ml: 0.5 }}
-                  />
-                </ListItem>
+                    <ListItemIcon sx={{ minWidth: 30, mt: 0.5 }}>
+                      <CheckCircleIcon sx={{ color: "black", fontSize: 18 }} />
+                    </ListItemIcon>
+                    <ListItemText
+                      primary={item}
+                      primaryTypographyProps={{ variant: "body2" }}
+                      sx={{ textAlign: "left", ml: 0.5 }}
+                    />
+                  </ListItem>
                 ))}
               </List>
             </Box>
